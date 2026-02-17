dac5f6c689da4847bfde83563a87b3ec551b62bf
Brijendra Tiwari <brijendratiwari993@gmail.com>
2026-02-17 19:09:57 +0530

    First Commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import {BrouserRouting} from "react-dom"
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  
+  
+  
+  <App />
+
+
+);
